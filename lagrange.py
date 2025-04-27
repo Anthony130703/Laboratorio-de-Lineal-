1e1748923ede9c77a8bbd439cfe50501e7fbfd16
Anthony <tonynarciso2003@gmail.com>
2025-04-26 22:17:25 -0500

    obtencion de los codigos de las otras interpolaciones

--- lagrange.py
+++ lagrange.py
@@ -1,38 +1,69 @@
+from sympy import symbols
+import matplotlib.pyplot as plt
 import numpy as np
-import matplotlib.pyplot as plt # Librerías
+from sympy import *
+
 # Definimos los puntos conocidos originalmente
-Q = np.array([500, 700, 900, 1100, 1300, 1500, 1700, 1900])  # Q(l/h)
-N = np.array([365, 361.6, 370.64, 379.68, 384.46, 395.5, 395.95, 397])  # N(w)
+Q = [500, 700, 900, 1100, 1300, 1500, 1700, 1900]  # X (Q(l/h))
+N = [365, 361.6, 370.64, 379.68, 384.46, 395.5, 395.95, 397]  # Y (N(w))
 
-# Definimos la función que calcula la interpolación de Lagrange en un punto x
-def lagrange_interpolation(x, X_points, Y_points):
-    total = 0 # inicializamos la suma del polinomio en 0
-    n = len(X_points)
-    # Sumamos cada término Y_i * L_i(x) 
-    for i in range(n):
-        term = Y_points[i] # Empezamos con el coeficiente Y_i
-        # Calculamos el producto para L_i(x)
-        for j in range(n):
-            if i != j: # Evitamos dividir entre 0
-                term *= (x - X_points[j]) / (X_points[i] - X_points[j])
-        total += term #Sumamos el término al polinomio
-    return total # Devolvemos el valor interpolado en x
-# Creamos un conjunto de puntos cercanos donde evaluaremos el polinomio
-x_accumulation = np.linspace(min(Q), max(Q), 500)
+# Definir la variable simbólica x que usaremos en el álgebra
+x = symbols('x')
+
+# Inicializamos una lista para almacenar los valores L_i(x)
+L = []
+# Calculamos los términos L_i(x) para cada valor de i (Cada punto)
+for i in range(len(Q)):
+    num = 1  # Numerador de L_i(x)
+    den = 1  # Denominador de L_i(x)
+    # Calculamos la productoria para el numerador y denominador
+    for j in range(len(Q)):
+        if i != j:  # Evitamos dividir entre 0
+            num *= (x - Q[j])
+            den *= (Q[i] - Q[j])
+    # Calculamos L_i(x) y lo multiplicamos por el valor correspondiente de y_i
+    L.append(N[i] * num / den)
+
+# El polinomio es la suma de estos productos simbólicos
+polynomial = sum(L)
+
+# Mostramos el polinomio usando LaTeX sin expandir los productos
+from IPython.display import display, Math
+
+# Construimos manualmente el polinomio en LaTeX
+latex_terms = []
+n = len(Q)
+for i in range(n):
+    num_factors = []
+    den_factors = []
+    for j in range(n):
+        if i != j:
+            num_factors.append(f"(x-{Q[j]})")
+            den_factors.append(f"({Q[i]}-{Q[j]})")
+    num_str = ''.join(num_factors)
+    den_str = ''.join(den_factors)
+    term_latex = f"\\frac{{{num_str}}}{{{den_str}}}"
+    latex_terms.append(f"{N[i]} \\times {term_latex}")
+
+# Juntamos todos los términos
+polynomial_latex = " + ".join(latex_terms)
+
+# Mostramos el polinomio
+display(Math('P(x) = ' + polynomial_latex))
+
+# Generamos varios puntos cercanos para graficar
+x_vals = np.linspace(min(Q), max(Q), 400)
 # Evaluamos todos esos puntos cercanos con la función de interpolación
-y_accumulation = np.array([lagrange_interpolation(x, Q, N) for x in x_accumulation])
+y_vals = [float(polynomial.subs(x, val)) for val in x_vals]
 
-# Graficamos una figura que se imprimirá en pantalla
-plt.figure(figsize=(10, 6))
-# Graficamos la curva de interpolación
-plt.plot(x_accumulation, y_accumulation, label='Interpolación de Lagrange', color='blue')
-# Graficamos los puntos originales
-plt.scatter(Q, N, color='red', label='Puntos conocidos')
-# Añadimos etiquetas
-plt.title('Interpolación de Lagrange para los datos dados')
-plt.xlabel('Q (l/h)')
-plt.ylabel('N (w)')
-# Añadimos cuadrícula, leyenda y mostramos
-plt.grid(True)
+# Graficamos la curva
+plt.plot(x_vals, y_vals, label='Polinomio de Interpolación', color='blue')
+# Graficamos los puntos conocidos originalmente
+plt.scatter(Q, N, color='red', label='Puntos de Interpolación')
+# Añadimos Etiquetas
+plt.title('Interpolación de Lagrange')
+plt.xlabel('Q(l/h)')
+plt.ylabel('N(w)')
 plt.legend()
+plt.grid(True)
 plt.show()
